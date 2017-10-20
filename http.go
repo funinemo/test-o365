56edac79d94e8ba023ec9a7b824c76e594e293ca
kanamori <kanamori@tsunagunet.com>
2017-10-20 17:35:27 +0900

    server debug message and get request parameter

--- http.go
+++ http.go
@@ -8,13 +8,32 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		//		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+		log.SetFlags(log.Lmicroseconds)
+		log.Println("=====START=====")
+		log.SetFlags(log.Lshortfile)
 		t := template.Must(template.ParseFiles("test.html"))
 		err := t.ExecuteTemplate(w, "test.html", nil)
 		if err != nil {
 			log.Fatal(err)
 		}
-		log.Printf("handler connect\n")
+		log.Printf("%v\n",r)
+		log.Printf("%#v\n",r)
+		log.Printf("Method: %s\n", r.Method)
+		log.Printf("%v,%T,%#v\n",r.URL,r.URL,r.URL)
+		log.Printf("Path: %s\n",r.URL.Path)
+		log.Printf("RemoteAddr: %v\n", r.RemoteAddr)
+		log.Printf("RequestURI: %v\n", r.RequestURI)
+		log.Printf("Host: %v\n", r.Host)
+    if err := r.ParseForm(); err != nil {
+      log.Fatal(err)
+    }
+    for k,v := range r.Form {
+      log.Printf("%s: %s\n", k,v)
+    }
+
+		for k,v := range r.Header {
+			log.Printf("%s: %s\n",k,v)
+		}
 	})
 	log.Printf("start http server\n")
 	err := http.ListenAndServe("10.1.208.163:8080", nil)
--- https.go
+++ https.go
@@ -8,13 +8,35 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		//		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+		log.SetFlags(log.Lmicroseconds)
+		log.Println("=====START=====")
+		log.SetFlags(log.Lshortfile)
 		t := template.Must(template.ParseFiles("test.html"))
 		err := t.ExecuteTemplate(w, "test.html", nil)
 		if err != nil {
 			log.Fatal(err)
 		}
-		log.Printf("handler connect\n")
+		log.Printf("%v\n",r)
+		log.Printf("%#v\n",r)
+    log.Printf("Method: %s\n", r.Method)
+    log.Printf("%v,%T,%#v\n",r.URL,r.URL,r.URL)
+    log.Printf("Path: %s\n",r.URL.Path)
+    log.Printf("RemoteAddr: %v\n", r.RemoteAddr)
+    log.Printf("RequestURI: %v\n", r.RequestURI) // ここで取れる
+    log.Printf("Host: %v\n", r.Host)
+    log.Printf("RawQuery: %s\n", r.URL.RawQuery) // ここでも取れる
+
+		// だがしかし、ここで取るのが一般的だろう
+		if err := r.ParseForm(); err != nil {
+			log.Fatal(err)
+		}
+		for k,v := range r.Form {
+			log.Printf("%s: %s\n", k,v)
+		}
+			
+		for k,v := range r.Header {
+			log.Printf("%s: %s\n", k,v)
+		}
 	})
 	log.Printf("start https server\n")
 	err := http.ListenAndServeTLS("10.1.208.163:8443", "ssl/service.crt","ssl/service.key",nil)
